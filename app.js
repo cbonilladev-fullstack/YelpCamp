5f69125d4bd5e9ec3e726e351bb2fcfbfab582c1
cabonilla <christian.bonilla01@gmail.com>
2019-06-16 21:56:26 +0000

    Example commit.

--- app.js
+++ app.js
@@ -52,6 +52,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds/:id/reviews", reviewRoutes);
 
-app.listen(process.env.PORT, function()  {
+app.listen(process.env.PORT, process.env.IP, function()  {
     console.log("Your Yelpcamp server is served!");
 });
